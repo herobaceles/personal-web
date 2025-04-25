f420a6b4e91beb754ad5923025514821c9b36bdb
herobaceles <hero.baceles.cln@gmail.com>
2025-04-26 04:38:07 +0800

    Your commit message here

--- portfolio-react-tailwind/src/components/HeroPanel.jsx
+++ portfolio-react-tailwind/src/components/HeroPanel.jsx
@@ -12,14 +12,21 @@ export default function HeroPanel() {
        <div className="flex items-center justify-center">
        <div className=" grid grid-cols-5 grid-rows-4 gap-2.5  w-200 h-120  container">
 
-            <div className="border  border-[#eaeaea] rounded-xl col-span-5 row-span-2 shadow">
-                <h1> </h1>
+            <div className=" border-[#eaeaea] rounded-xl col-span-5 row-span-2 shadow flex">
+                    <div className="flex-1  p-5">
+                        <h3 className="font-bold text-lg">About</h3>
+                        <p className="text-xs leading-6">I'm a backend software developer  specializing in building robust and scalable solutions using the MERN stack, PHP, and MySQL. I work on projects that involve designing secure APIs, managing complex databases, optimizing server performance, and integrating backend systems that power modern websites, web applications, and mobile platforms.</p></div>
+                    <div className="flex-auto border"></div>
             </div>
             <div className="border  border-[#eaeaea] col-span-3 rounded-xl row-span-2 shadow">
-                <h1> </h1>
+            <div className="flex-1  p-5">
+            <h3 className="font-bold text-lg">Techstack</h3>
+            </div>
+
             </div>
             <div className="border  border-[#eaeaea] col-span-2 row-span-2 rounded-xl shadow">
-                 
+            <div className="p-5"><h3 className="font-bold text-lg">Connect</h3> </div>
+            
             </div>
         </div>
         
--- portfolio-react-tailwind/src/components/NavBar.jsx
+++ portfolio-react-tailwind/src/components/NavBar.jsx
@@ -6,10 +6,28 @@ export default function NavBar() {
 
 
     return(
-        <header className="bg-white  ">
-            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-center p-6 md:px-8">
+        <header className="bg-white flex justify-center align-center my-5">
+            <nav aria-label="Global" className="flex justify-center align-center w-200 container">
+                <div className=" flex flex-auto py-2">
+                <div className=''>
+                   <img src={profile} alt="profle photo" className='w-30 h-30 rounded-lg  border-[#eaeaea] shadow me-4' />
+                   </div>
                 
-                <div className="flex lg:flex-1  justify-center">
+                <a href="#" className="-m-1.5 p-1.5"> 
+                        <span className="font-bold text-2xl " >Hero B </span><br />
+                        <span className='text-sm'>Backend Developer/ Student</span> <br />
+                        <span className='text-xs'>Manila, Philippines</span> <br />
+                        <button class=" flex items-center justify-center px-4 py-4 h-8 w-25 bg-black text-white rounded my-1 text-xs">
+                         Send Email
+                        </button>
+                    </a>
+                </div>
+                <div className="flex-auto"><h1></h1></div>
+
+
+
+                
+                {/* <div className="flex lg:flex-1  justify-center">
 
 
                    <div className='mx-5'>
@@ -25,7 +43,7 @@ export default function NavBar() {
                          Send Email
                         </button>
                     </a>
-                </div>
+                </div> */}
                  
                
             </nav>
